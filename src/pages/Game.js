347be7c930ef0033d353467f2b51cdf450a84ba0
gagev <gagevank@gmail.com>
2021-12-24 16:09:08 -0500

    Fixed Timer issue -- not displaying the right format of the time 00:000

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,45 +4,39 @@ import { Strong } from '../styled/Strong';
 
 export default function Game({history}) {
     const [score, setScore] = useState(1);
-    const MAX_SECONDS = 5;
+    const MAX_SECONDS = 30;
     const [ms, setMs] = useState(0);
     const [seconds, setSeconds] = useState(MAX_SECONDS);
 
     useEffect (() => {
         const currentTime = new Date();
-        const interval = setInterval(() => updateTime(currentTime), 1);
+        const interval = setInterval( () => updateTime(currentTime), 1);
         return () => clearInterval(interval);
     }, []);
 
     const updateTime = (startTime) => {
         const endTime = new Date();
         const msPassedStr = ( endTime.getTime() - startTime.getTime()).toString();
-        const formattedMSString = ('0000' + msPassedStr).slice(-5);
-        const updatedSeconds = MAX_SECONDS - parseInt(formattedMSString.substring(0,2)) - 1;
-        const updatedMs = 1000 - parseInt(formattedMSString.substring(formattedMSString.length - 3));
-        setSeconds(addLeadingZeros(updatedSeconds, 2));
+        const formttedMsString = ('0000' + msPassedStr).slice(-5);
+        const updatedSeconds = MAX_SECONDS - parseInt(formttedMsString.substring(0, 2));
+        const updatedMs = 1000 - parseInt(formttedMsString.substring(formttedMsString.length - 3));
+        setSeconds(addLeadingZeros(updatedSeconds, 2))
         setMs(addLeadingZeros(updatedMs, 3));
     };
 
-    useEffect(() => {
-        if (seconds <= -1){
-            history.push('/gameOver');
-        }
-    }, [seconds, ms]);
-
-    const addLeadingZeros = (str, length) => {
-        let zeros = ' ';
+    const addLeadingZeros = (num, length) => {
+        let zeros = '';
         for(let i = 0; i < length; i++){
-            zeros += "0";
+            zeros += "0"
         }
-        return (zeros + str).slice(-length);
+        return (zeros + num).slice(-length);
     }
 
     return (
         <StyledGame>
             <StyledScore>Score: <Strong>{score}</Strong></StyledScore>
             <StyledChar>A</StyledChar>
-            <StyledTimer>Time: <Strong>{seconds}: {ms}</Strong></StyledTimer>
+            <StyledTimer>Time:{''} <Strong>{seconds}: {ms}</Strong></StyledTimer>
         </StyledGame>
     );
 }
