5047bdbed7803654fafae9189c6cf44240c6c2b1
gagev <gagevank@gmail.com>
2021-12-24 16:15:05 -0500

    Fixed issue with timer not stopping at 0 - also sends used to Game Over page once the timer is done

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,7 +4,7 @@ import { Strong } from '../styled/Strong';
 
 export default function Game({history}) {
     const [score, setScore] = useState(1);
-    const MAX_SECONDS = 30;
+    const MAX_SECONDS = 2;
     const [ms, setMs] = useState(0);
     const [seconds, setSeconds] = useState(MAX_SECONDS);
 
@@ -24,13 +24,20 @@ export default function Game({history}) {
         setMs(addLeadingZeros(updatedMs, 3));
     };
 
+    useEffect ( () => {
+        if(seconds <= -1){
+            history.push('/gameOver');
+        }
+    
+    }, [seconds, ms]);
+
     const addLeadingZeros = (num, length) => {
         let zeros = '';
         for(let i = 0; i < length; i++){
             zeros += "0"
         }
         return (zeros + num).slice(-length);
-    }
+    };
 
     return (
         <StyledGame>
