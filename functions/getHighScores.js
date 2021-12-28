726e614bcd5a47cfaa96fd61a9afe0a435517d9c
gagev <gagevank@gmail.com>
2021-12-28 17:20:07 -0500

    fixing highscore function

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -2,28 +2,28 @@ require('dotenv').config();
 const Airtable = require('airtable');
 
 Airtable.configure({
-    apiKey: process.env.AIRTABLE_API_KEY,
+    apiKey: process.env.AIRTABLE_API_KEY
 });
 
-const base = require('airtable').base(process.env.AIRTABLE_API_BASE);
-const table = base(process.env.AIRTABLE_API_TABLE);
+const base = require('airtable').base(process.env.AIRTABLE_API_BASE)
+const table = base(process.env.AIRTABLE_API_TABLE)
 
 exports.handler = async (event) => {
 try{
     const records = await table.select().firstPage();
     const formattedRecords = records.map((record) => ({
         id: record.id,
-        fields: record.fields,
+        fields: record.fields
     }));
 
     return {
         statusCode: 200,
-        body: JSON.stringify(formattedRecords),
+        body: JSON.stringify(formattedRecords)
     };
 }catch (err) {
     return {
         statusCode: 500,
-        body: JSON.stringify({err: "Error"}),
+        body: JSON.stringify({err: "Error"})
          };
 
     }
