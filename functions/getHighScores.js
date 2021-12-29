03c2c75b0f6ddbb4924b7f8deee3583110c2cd09
gagev <gagevank@gmail.com>
2021-12-29 17:36:34 -0500

    formatting record output

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -7,11 +7,22 @@ const base = Airtable.base('appKbu6WMQiKJEvAx');
 const table = base.table("Table1");
 
 exports.handler = async (event, context) => {
-
+    try{
     const records = await table.select({}).firstPage();
+    const formattedRecords = records.map((record) => ({
+        if: record.id,
+        fields: record.fields,  
+    }));
 
     return {
         statusCode: 200,
-        body: JSON.stringify(records),
+        body: JSON.stringify(formattedRecords),
     };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({err: "Records not found"}),
+        };
+    }
+
 };
