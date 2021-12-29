937857afde49139d45b87cb0d3362e8143ba1cea
gagev <gagevank@gmail.com>
2021-12-29 16:49:56 -0500

    Trying to get function to work

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -1,21 +1,17 @@
-require('dotenv').config();
-const Airtable = require('airtable');
+var Airtable = require('airtable');
 Airtable.configure({
-    apiKey: process.env.AIRTABLE_API_KEY
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'key4cFwk1ynHHzTTL'
 });
-const base = Airtable.base(process.env.AIRTABLE_BASE);
-const table = base.table(process.env.AIRTABLE_TABLE);
+const base = Airtable.base('appKbu6WMQiKJEvAx');
+const table = base.table('TypingGame');
 
-exports.handler = async (event) => {
-   
-        const records = await table.firstPage();
-        const formattedRecords = records.map((record) => ({
-            id: record.id,
-            fields: record.fields
-        }));
-        return {
-            statusCode: 200,
-            body: JSON.stringify(formattedRecords)
-        };
-    
+exports.handler = async (event, context) => {
+
+    const records = await table.select({}).firstPage();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(records),
+    };
 };
